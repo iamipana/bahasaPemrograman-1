4b7d691e512360b107632a61e8ee09dec8a4d691
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:49:12 +0700

    commit coba

---
+++ src/main/kotlin/Latihan2a.kt
@@ -0,0 +1,8 @@
+fun main (args: Array<String>) {
+    var namaLengkap: String = "Sari Nur"
+    var alamat: String  = "Sukoharjo"
+    var umur = 19
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
+}
